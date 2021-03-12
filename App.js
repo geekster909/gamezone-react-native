0cfc6122a33e981d9fdef150f4945f0c12076f57
Justin Bond <bond.justink@gmail.com>
2021-03-12 11:51:10 -0800

    review app started && custom fonts loaded

--- App.js
+++ App.js
@@ -1,83 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Alert, FlatList, Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoItem';
-import AddTodo from './components/addTodo';
-import Sandbox from './components/sandbox';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import Home from './screens/home';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1' },
-    { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' },
-  ]);
+  let [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
 
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key);
-    });
-  };
-
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodos((prevTodos) => {
-        return [
-          {
-            text: text,
-            key: Math.random().toString()
-          },
-          ...prevTodos
-        ]
-      });
-    } else {
-      Alert.alert('OOPS!', 'Todos must be over 3 chars long.', [
-        {
-          text: 'Understood',
-          onPress: () => {
-            console.log('alert closed');
-          }
-        },
-      ])
-    }
-  };
+  if (!fontsLoaded) {
+    console.log('loading fonts...');
+    return <AppLoading />;
+  }
 
   return (
-    // <Sandbox />
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-      console.log('dismissed keyboard');
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    </TouchableWithoutFeedback>
+    <Home />
   );
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-  },
-  list: {
-    flex: 1,
-    marginTop: 20.
-  },
-});
---
+++ screens/reviewDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default function ReviewDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>ReviewDetails screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+});
